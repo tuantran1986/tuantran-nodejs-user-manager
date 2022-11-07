cfc7406f32f4d1560d69c88e87f97301be8ae135
tuanta <tuantran.kum@gmail.com>
2022-11-07 22:05:55 +0700

    bai 2 - ket noi DATABASE + READ

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// 1. require 2.connect 3.schema 4.model
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/webSale');  // "tên DB" = webSale
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+});                                                     // "SCHEMA" : cấu trúc 1 DOC
+const userModel = mongoose.model('users', userSchema);  // "MODEL": quản lý COLLECTION USER
+
+
+
 // CONFIG - PUG
 app.set('views', './views');    // LINK đến thư mục ./VIEWS
 app.set('view engine', 'pug');  // thiết lạp VIEW ENGINE = PUG
@@ -17,16 +29,20 @@ app.get('/', (req, res) => {
     res.render('index', { pageName: 'HomePage' });
 })
 
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
+
+    const userList = await userModel.find({});      // ĐỌC DỮ LIỆU - TỪ : "DATABASE"
 
     // RES.RENDER - trả về : "PAGE_HTML"
         // TS1: nội dung trong địa chỉ = "views/users/index.pug"
         // TS2: truyền "mảng listUsers" vào VIEWS: bằng toán tử {}
-    res.render('users/index', { listUsers: [
-        { id: 1, name: 'tuantran'},
-        { id: 2, name: 'thanhthu'},
-        { id: 3, name: 'quocanh'}
-    ]});
+    res.render('users/index', { listUsers: userList});
+
+    // res.render('users/index', { listUsers: [
+    //     { id: 1, name: 'tuantran'},
+    //     { id: 2, name: 'thanhthu'},
+    //     { id: 3, name: 'quocanh'}
+    // ]});
 })
 
 
