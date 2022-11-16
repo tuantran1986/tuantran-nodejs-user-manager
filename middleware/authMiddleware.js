107731f970d785597efae7a76a14068e3c93e938
tuanta <tuantran.kum@gmail.com>
2022-11-17 06:30:50 +0700

    bai 16 -3- sua giao dien LOGIN - LOGOUT

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 const userModel = require("../models/userModel");
 
-module.exports.authRequire = (req, res, next) => {
+module.exports.authRequire = async (req, res, next) => {
     console.log('ĐỌC COOKIES = req.cookies', req.cookies);
 
     if (!req.cookies.userId) {
@@ -10,12 +10,18 @@ module.exports.authRequire = (req, res, next) => {
 
     // req.cookies.userId : có giá trị là USER_ID - đc GHI VÀO BROWSER khi LOGIN THÀNH CÔNG
     // check trong DB- nếu không có ID đó - thì REDIRECT về trang LOGIN !
-    const currentUser = userModel.find({ _id: req.cookies.userId });
+                // cydb - ĐỌC DỮ LIỆU:          DATABASE = "ASYNC + AWAIT"
+    const currentUser = await userModel.findOne({ _id: req.cookies.userId });
 
     if (!currentUser) {
         res.redirect('/auth/login');
         return;
     }
+
+    // truyền USER từ CONTROLLER sang VIEW-LAYOUT: để hiển thị trên NAV = "res.locals" là BIẾN TOÀN CỤC - bản chất OBJECT.
+    res.locals.currentUserLocal = currentUser;
+        // console.log('currentUser', currentUser);
+        // console.log('res.locals.currentUserLocal', res.locals.currentUserLocal);
 
     next();
 }
