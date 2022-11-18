26747ccc7f4fb20fdb7d6604705b5e3767f2daa2
tuanta <tuantran.kum@gmail.com>
2022-11-18 17:26:30 +0700

    bai 20 -3- phan trang FE dong theo BE

--- controllers/productController.js
+++ controllers/productController.js
@@ -21,5 +21,21 @@ module.exports.getProductPaginationController = async (req, res, next) => {
     const productList = await productModel.find({}).skip(skip).limit(limit);
     // console.log('productList', productList);
 
-    res.render('products/pagination', { productList: productList || [] });
+    // 1.ĐẾM SỐ LƯỢNG PHẦN TỬ:
+    const countDocuments = await productModel.countDocuments({});
+    const countPages = Math.floor(countDocuments / rowPerPage) || 1;    // Math.floor: làm tròn - xuống nguyên
+    // console.log('countDocuments = ', countDocuments);
+
+    // 2. TRUYỀN BIẾN - VÀO VIEW: "queryParams = countPages + currentPage"
+    const queryParams = {
+        countPages: countPages,
+        currentPage: page || 1
+        // currentRowPerPage: rowPerPage || 8 : để 8 sản phẩm 1 trang
+    };
+
+    // truyền vào VIEW:
+        // productList
+        // queryParams
+    res.render('products/pagination', { productList: productList || [], queryParams: queryParams });
+
 }
