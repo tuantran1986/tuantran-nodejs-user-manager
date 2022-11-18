70e24e9af589a08a2e97c92141ed7928bc897e8d
tuanta <tuantran.kum@gmail.com>
2022-11-18 15:46:30 +0700

    bai 20 -2- phan trang FIXED CODE FE

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,3 +5,21 @@ module.exports.getAllProductController = async (req, res, next) => {
 
     res.render('products/index', { productList: productList || [] });
 }
+
+module.exports.getProductPaginationController = async (req, res, next) => {
+
+    // console.log('req.query', req.query);
+    const page = req.query?.page || 1;  // mac dinh = 1
+    const rowPerPage = 8;
+
+    const skip = (page -1) * rowPerPage;
+    const limit = rowPerPage;
+
+    // PHÂN TRANG = FIND.SKIP.LIMIT
+        // SKIP: bỏ qua bao nhiêu phần tử
+        // LIMIT: số phần tử 1 trang
+    const productList = await productModel.find({}).skip(skip).limit(limit);
+    // console.log('productList', productList);
+
+    res.render('products/pagination', { productList: productList || [] });
+}
--- routers/productRouter.js
+++ routers/productRouter.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { getAllProductController } = require('../controllers/productController');
+const { getAllProductController, getProductPaginationController } = require('../controllers/productController');
 // sử dụng EXPRESS.ROUTER
 const productRouter = express.Router();
 
 
 // 1.lấy tất cả product
 productRouter.get('/all', getAllProductController);
+// 2.phân trang: product
+productRouter.get('/pagination', getProductPaginationController);
 
 
 // nhớ MODULE.EXPORTS (có S) : USER_ROUTER
