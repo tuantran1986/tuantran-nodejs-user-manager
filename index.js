c406947468e80bb9d844b8da515dac93af6bc606
tuanta <tuantran.kum@gmail.com>
2022-12-28 11:16:12 +0700

    bai 29- sua truoc khi deploy to heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 // CẤU HÌNH - "COOKIE THƯỜNG"
--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 // 1. require 2.connect 3.schema 4.model
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/webSale');  // "tên DB" = webSale
+mongoose.connect(process.env.MONGO_URL);  // "tên DB" = webSale
+// mongoose.connect('mongodb://localhost:27017/webSale');  // "tên DB" = webSale
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
