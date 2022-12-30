5d992ae04870b4ae399069230e73edc3217d3342
tuanta <tuantran.kum@gmail.com>
2022-12-30 19:18:46 +0700

    tuantran nodeJs and style

--- index.js
+++ index.js
@@ -5,7 +5,9 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; 
+
+
 
 
 // CẤU HÌNH - "COOKIE THƯỜNG"
